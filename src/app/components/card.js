2217325b48e0735ba0ae644a750b1baa9840b4eb
DanielRamirezGe <42500282+DanielRamirezGe@users.noreply.github.com>
2025-02-15 20:28:25 -0800

    Add card house v1

---
+++ src/app/components/card.js
@@ -0,0 +1,99 @@
+"use client";
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import HouseIcon from "@mui/icons-material/House";
+import BedIcon from "@mui/icons-material/Bed";
+import ShowerIcon from "@mui/icons-material/Shower";
+import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
+import Box from "@mui/material/Box";
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme }) => ({
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+  variants: [
+    {
+      props: ({ expand }) => !expand,
+      style: {
+        transform: "rotate(0deg)",
+      },
+    },
+    {
+      props: ({ expand }) => !!expand,
+      style: {
+        transform: "rotate(180deg)",
+      },
+    },
+  ],
+}));
+
+export default function CardHouse(props) {
+  const { houseDetails } = props;
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        title={houseDetails.title}
+        subheader={houseDetails.subheader}
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image={houseDetails.image}
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          $ 1,012,700
+        </Typography>
+        <Typography variant="body3" color="text.secondary">
+          <Box display="flex" alignItems="center">
+            <HouseIcon /> {houseDetails.size} m2 | <BedIcon />{" "}
+            {houseDetails.bed} | <ShowerIcon /> {houseDetails.shower} |{" "}
+            <DirectionsCarIcon /> {houseDetails.car}
+          </Box>
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography sx={{ marginBottom: 2 }}>
+            {houseDetails.description}
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,41 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import CardHouse from "./components/card";
 
 export default function Home() {
+  const houseDetails = [
+    {
+      title: "Los Héroes San Pablo",
+      subheader: "Tecámac - Estado de México",
+      image: "house_test/los-heroes-san-pablo-sauce.jpg",
+      description:
+        "Los Héroes San Pablo es un proyecto de vivienda de interés social en el municipio de Bello, Antioquia. Cuenta con apartamentos de 2 y 3 alcobas, con áreas desde 47 m2 hasta 65 m2. El proyecto cuenta con zonas comunes como piscina, juegos infantiles, gimnasio, cancha múltiple, salón social, entre otros.",
+      bed: 2,
+      shower: 1,
+      car: 1,
+      size: 57.41,
+      key: 1213123,
+    },
+    {
+      title: "Citara",
+      subheader: "Huehuetoca - Estado de México",
+      image: "house_test/citara.png",
+      description:
+        "Disfruta de una casa amplia pensada en tu familia, con área de construcción de 67.1m2, 3 recámaras en total y 1.5 baños. Pasa tiempo con tu familia en el jardín trasero de 6x4m2 y recibe a tus invitados desde un atractivo frente de vivienda de 6m x 16m de fondo. Tu próximo hogar se entrega además con piso de vinil, boiler, tarja y lavadero, así como cocina equipada y espacios para los clósets. Todo el desarrollo está cercado y cuenta con caseta de vigilancia para tu máxima seguridad. ",
+      bed: 3,
+      shower: 1.5,
+      car: 1,
+      size: 67.1,
+      key: 12131223,
+    },
+  ];
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        {houseDetails.map((house) => (
+          <CardHouse houseDetails={house} key={house.key} />
+        ))}
+        {/* <Card /> */}
         <Image
           className={styles.logo}
           src="/next.svg"
