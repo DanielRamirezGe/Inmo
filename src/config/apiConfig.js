0c3025d357e1c34241be7da8ad4ff593c38a7e60
DanielRamirezGe <42500282+DanielRamirezGe@users.noreply.github.com>
2025-08-24 22:07:31 -0700

    delete local test

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -2,7 +2,7 @@ const isDevelopment = process.env.NODE_ENV === "development";
 
 const apiConfig = {
   baseURL: isDevelopment
-    ? `http://localhost:3010`
+    ? `https://adonaipayment.com/minkaas_test`
     : "https://adonaipayment.com/minkaasa",
   whatsAppPhoneNumber: "+525651562698", // Número de WhatsApp para contacto
   facebookPageId: "61573640081107", // ID de la página de Facebook para Messenger
