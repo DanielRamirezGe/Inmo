5914ca5a5b1efa093522d6e68acbf56bd263a961
DanielRamirezGe <42500282+DanielRamirezGe@users.noreply.github.com>
2025-08-24 22:11:29 -0700

    add minkaasa test

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -2,7 +2,7 @@ const isDevelopment = process.env.NODE_ENV === "development";
 
 const apiConfig = {
   baseURL: isDevelopment
-    ? "https://adonaipayment.com/minkaas_test"
+    ? "https://adonaipayment.com/minkaasa_test"
     : "https://adonaipayment.com/minkaasa",
   whatsAppPhoneNumber: "+525651562698", // Número de WhatsApp para contacto
   facebookPageId: "61573640081107", // ID de la página de Facebook para Messenger
